6311a6f8cb54578bb4607feeb0a4e92e9674caa5
Paci Salminen <pasi.salminen@cgi.com>
2022-11-03 20:29:41 +0200

    ny wörnii

--- app.js
+++ app.js
@@ -2,18 +2,27 @@ const express = require('express');
 const app = express();
 const { expressjwt: jwt } = require('express-jwt');
 const { readFileSync } = require('fs');
+const debug = require('debug')('app');
 
 const fileName = './public-key.pem';
 const contents = readFileSync(fileName, 'utf-8');
 
-app.get(
-  "/protected",
-  jwt({ secret: contents, algorithms: ["RS256"] }),
-  function (req, res) {
-    if (!req.auth.admin) return res.sendStatus(401);
-    res.send('Hello World!');
-  }
-);
-console.log('Listening')
-app.listen(6666);
+app.use(jwt({ secret: contents, algorithms: ["RS256"] }));
+
+app.get('/protected', (req, res) => {
+    res.send(`Hello, ${req.user.sub}`);
+});
+
+// Set port from env or by default 8080
+const port = +(process.env.PORT || '8080');
+const server = app.listen(port, () => {
+  debug(`Running on ${port}`);
+});
+
+process.on('SIGTERM', () => {
+  debug('SIGTERM signal received, closing the HTTP server');
+  server.close(() => {
+    debug('HTTP server closed');
+  });
+});
 
